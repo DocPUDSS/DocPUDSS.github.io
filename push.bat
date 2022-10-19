32120b2b4f64f2109eff7336cbbc1f5cd3a00c75
cit142g-31@M306.UC-BCF.EDU.PH <cit142g-31@M306.UC-BCF.EDU.PH>
2022-10-19 15:17:47 +0800

    Auto upload

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 echo set Remote URL 
-git remote set-url origin https://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@github.com/DocPUDSS/DocPUDSS.github.io
+git remote set-url origin https://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@github.com/DocPUDSS/DocPUDSS.github.io
 pause
 echo ADD, commit push
 git add --all
